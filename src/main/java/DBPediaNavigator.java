75280a65a17178993a9ca8a37247c113aff47d4b
Lukas Loos <l.loos95@gmail.com>
2020-10-28 18:34:48 +0100

    Add javadoc

--- src/main/java/DBPediaNavigator.java
+++ src/main/java/DBPediaNavigator.java
@@ -13,8 +13,9 @@ public class DBPediaNavigator {
     String DBPEDIA_SPARQL_ENDPOINT = "http://dbpedia.org/sparql";
     String SPARQL_PREFIXES = "PREFIX dbr: <" + RESOURCE_URI + "> PREFIX dbo: <" + ONTOLOGY_URI + "> ";
 
-
+    /** Model containing all valid triples belonging to previously seen resources. */
     protected Model memoryModel;
+    /** Set of Resources that were already seen by the user. */
     protected Set<String> previousResources;
 
     public DBPediaNavigator() {
@@ -22,6 +23,12 @@ public class DBPediaNavigator {
         previousResources = Sets.newHashSet();
     }
 
+    /**
+     * Loads all triples belonging to the provided resource from DBPedia and adds the valid ones to the 
+     * memoryModel. Valid triples are those whose predicate is not dbo:wikiPageWikiLink (because the meaning of these
+     * triples is rather limited) and whose subject and object both are from DBPedia.
+     * @param newResource Name of the new resource to register
+     */
     public void registerNewResource(String newResource) {
         Query describeQuery = QueryFactory.create(SPARQL_PREFIXES + "DESCRIBE dbr:" + newResource);
         QueryExecution describeExecution = QueryExecutionFactory.sparqlService(DBPEDIA_SPARQL_ENDPOINT, describeQuery);
@@ -37,6 +44,12 @@ public class DBPediaNavigator {
         previousResources.add(newResource);
     }
 
+    /**
+     * Gets the resources from the memory graph ordered by the number of previously seen resources they are connected
+     * to.
+     * @return List of QuerySolutions containing the names of the resources (?new_word) and the number of previously
+     * seen resources the resources are connected to (?occurrences)
+     */
     public List<QuerySolution> findNextDestinations() {
         String previousResourceFilter = "FILTER(?new_word Not IN (dbr:" + String.join(", dbr:", previousResources) + "))";
         Query orderQuery = QueryFactory.create(SPARQL_PREFIXES +
