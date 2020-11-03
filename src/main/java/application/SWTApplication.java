8a934c21281995afef5bb0dd5c50e072d7664b03
Jonathan Sobbe <jsobbe@mail.uni-mannheim.de>
2020-11-03 19:43:31 +0100

    #15 Prepared project for JavaFX Application

---
+++ src/main/java/application/SWTApplication.java
@@ -0,0 +1,66 @@
+package application;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * The {@link Application} class that starts the app and initializes all
+ * relevant instances of {@link Scene}s and their controller-classes.
+ */
+public class SWTApplication extends Application {
+
+	/**
+	 * Starts the {@link Application}.
+	 * 
+	 * @param args Optional arguments. (Not used)
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	private Stage window;
+	/**
+	 * The main {@link Scene} of the application, where the user receives proposals
+	 * and interacts with the knowledge model.
+	 */
+	private Scene mainScene;
+	/**
+	 * Controller class for the main scene.
+	 */
+	private Initializable mainController;
+
+	public Initializable getMainController() {
+		return mainController;
+	}
+
+	/**
+	 * This method loads the {@link Scene}s from fxml and the controllers.
+	 * 
+	 * @throws IOException
+	 */
+	private void loadScenes() throws IOException {
+		// Main Scene
+		FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/app.fxml"));
+		mainScene = new Scene(loader.load());
+		mainController = loader.getController();
+	}
+
+	@Override
+	public void start(Stage window) throws Exception {
+
+		try {
+			loadScenes();
+		} catch (IOException e) {
+			System.err.println("Something went wrong while loading the Scenes: ");
+			e.printStackTrace();
+		}
+
+		window.setScene(mainScene);
+		window.show();
+	}
+
+}
---
+++ src/main/java/controller/MainSceneController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+
+/**
+ * Controller class of the main {@link Scene} of the app. Handles every
+ * interaction with the ui in this scene.
+ * 
+ */
+public class MainSceneController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+	}
+
+}
