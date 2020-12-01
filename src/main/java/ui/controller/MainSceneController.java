9a9f51b5172f31cb11a2ba022a653a7e5a61639b
Jonathan Sobbe <jsobbe@mail.uni-mannheim.de>
2020-12-01 23:14:12 +0100

    #29 Tasks and ProgressIndicators work now

--- src/main/java/ui/controller/MainSceneController.java
+++ src/main/java/ui/controller/MainSceneController.java
@@ -7,7 +7,7 @@ import java.util.ResourceBundle;
 import application.SWTApplication;
 import backend.TopicManagerImpl;
 import backend.exception.InvalidUriInputException;
-import javafx.concurrent.Service;
+import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -47,6 +47,11 @@ public class MainSceneController implements Initializable {
 	private TextFlow textArea1;
 	@FXML
 	private TextField numberArea;
+	@FXML
+	private ProgressIndicator indicator;
+	public ProgressIndicator getProgressIndicator() {
+		return indicator;
+	}
 
 	private ProposedTopicList proposedTopicList;
 	private AcceptedTopicList acceptedTopicList;
@@ -59,48 +64,52 @@ public class MainSceneController implements Initializable {
 		this.proposedTopicList = new ProposedTopicList();
 		this.proposedTopicBase.getChildren().add(proposedTopicList.getRoot());
 		this.acceptedTopicBase.getChildren().add(acceptedTopicList.getRoot());
-		
-		ProgressIndicator progressIndicator = new ProgressIndicator();
-		MyService myService = new MyService();
+
 		// run search on enter
 		this.text1.setOnKeyPressed(e -> {
 			if (e.getCode().equals(KeyCode.ENTER)) {
-				//onClick();
-				myService.restart();
+				// onClick();
+				new Thread(createLoadTask()).start();
 			}
 		});
+		this.btn1.setOnAction(e -> {
+			new Thread(createLoadTask()).start();
+		});
+		indicator.progressProperty().unbind();
+		indicator.setVisible(false);
 		
-		progressIndicator.progressProperty().unbind();
-        progressIndicator.visibleProperty().bind(myService.runningProperty());
-        this.proposedTopicBase.getChildren().add(progressIndicator);
-        
-        
 		this.text1.requestFocus();
 		numberArea.setText(numOfRequestedSuggestions + "");
 		TextFieldConfigurator.configureNumericTextField(numberArea, MAX_NUM_OF_SUGGESTIONS);
 		TextFieldConfigurator.configureUrlTextField(text1);
 	}
 	
-	private class MyService extends Service<Void> {
-
-        @Override
-        protected Task<Void> createTask() {
-            return new Task<Void>() {
-                @Override
-                protected Void call() throws Exception {        
-                	onClick();
-                    return null;
-                }
-            };
-        }
-    }
-
-	public String onClick() {
-		String text = text1.getText();
-		setResult(text);
-		return text;
+	private Task<Void> createLoadTask(){
+		Task<Void> loadTask = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+				indicator.setVisible(true);
+				onClick();
+				return null;
+			}
+		};
+		loadTask.setOnSucceeded(e -> {
+			Platform.runLater(() -> {
+				fetchNewSuggestions();
+				indicator.setVisible(false);
+			});
+		});
+		loadTask.setOnFailed(e -> {
+			loadTask.getException().printStackTrace();
+		});
+		return loadTask;
 	}
 
+	public void onClick() {
+		String text = text1.getText();
+		setResult(text);
+	}
+	
 	/**
 	 * Add a new topic to the list of accepted topics.
 	 * 
@@ -120,12 +129,17 @@ public class MainSceneController implements Initializable {
 		try {
 			String resourceUrl = TopicManagerImpl.RESOURCE_URI + s;
 			String label = SWTApplication.getTopicManager().addResourceToTopics(resourceUrl);
-			TopicInfo info = new TopicInfo(resourceUrl, label, "", "", "", 0); //TODO add type of resource?
-			this.acceptedTopicList.addTopic(info);
+			Platform.runLater(() -> {
+				TopicInfo info = new TopicInfo(resourceUrl, label, "", "", "", 0);
+				this.acceptedTopicList.addTopic(info);
+			});
+			
 		} catch (InvalidUriInputException e) {
-			Alert a = new Alert(Alert.AlertType.ERROR, "If you have trouble entering a topic, check its Wikipedia-Url and copy everything after 'https://en.wikipedia.org/wiki/'.");
+			Alert a = new Alert(Alert.AlertType.ERROR,
+					"If you have trouble entering a topic, check its Wikipedia-Url and copy everything after 'https://en.wikipedia.org/wiki/'.");
 			a.initStyle(StageStyle.UNIFIED);
-			a.getDialogPane().getStylesheets().add(SWTApplication.class.getResource("/css/general.css").toExternalForm());
+			a.getDialogPane().getStylesheets()
+					.add(SWTApplication.class.getResource("/css/general.css").toExternalForm());
 			a.setHeaderText("The resource could not be found. ");
 			a.showAndWait();
 		}
@@ -136,18 +150,18 @@ public class MainSceneController implements Initializable {
 			Alert a = new Alert(Alert.AlertType.ERROR, "Please give a number");
 			a.showAndWait();
 		}
-		// vorher
+	}
+
+	public void fetchNewSuggestions() {
 		List<TopicInfo> result = SWTApplication.getTopicManager()
 				.getSuggestionsForPreviousResources(numOfRequestedSuggestions);
-
 		proposedTopicList.clearAndPopulateList(result);
-
 	}
-    
-	public ProposedTopicList getproposedTopicList(){
+
+	public ProposedTopicList getproposedTopicList() {
 		return this.proposedTopicList;
 	}
-	
+
 	public TopicInfo getProposedTopicInfo(String resourceUrl) {
 		return acceptedTopicList.getTopic(resourceUrl);
 	}
